43615d138099a1e1cb454962def2b92fca774916
ricardiobraga <rcardiobraga@gmail.com>
2023-10-22 21:13:49 -0300

    first commit

---
+++ src/test/java/me/dio/desafioaoirest/DesafioAoiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.desafioaoirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioAoiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
